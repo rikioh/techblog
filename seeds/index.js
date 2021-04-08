04dbd5cba40cb9362d0ef93ce83848394dc918c0
Rikio Campbell <rikiocamp@gmail.com>
2021-04-08 14:19:29 -0700

    updated index seed and user-seed

---
+++ seeds/index.js
@@ -0,0 +1,13 @@
+const seedUser = require('./user-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedUser();
+  console.log('\n----- Users SEEDED -----\n');
+  process.exit(0);
+};
+
+seedAll();
