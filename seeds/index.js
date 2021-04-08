76f5fddfe3b6fe6b5ca89c4fb7e49fab3cd69dad
Rikio Campbell <rikiocamp@gmail.com>
2021-04-08 15:10:16 -0700

    changed userdata seed to json file

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,16 @@
-const seedUser = require('./user-seeds');
+const { User } = require('../models');
 
 const sequelize = require('../config/connection');
+const userData = require('./userData.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUser();
+  //Create all the users.
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
   console.log('\n----- Users SEEDED -----\n');
   process.exit(0);
 };
