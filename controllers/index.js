98b2b9104a3ef758a044dc67fe6477d83262fe46
Rikio Campbell <rikiocamp@gmail.com>
2021-04-08 15:53:12 -0700

    updated index js to include homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 //Imports.
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 //Define routes.
 router.use('/api', apiRoutes);
+router.use('/api', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
