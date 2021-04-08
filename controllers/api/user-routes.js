5493583e3aa77e7d74b4c426694f4de4cd213dd1
Rikio Campbell <rikiocamp@gmail.com>
2021-04-08 14:28:09 -0700

    created user-routes api with get,post,delete

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET a single user by ID
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No user found with that ID' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST - Create a user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(userData => {
+            req.session.save(() => {
+                req.session.user_id = userData.id;
+                req.session.username = userData.username;
+                req.session.loggedIn = true;
+
+                res.json(userData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE - delete a user
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No user found with that ID' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
